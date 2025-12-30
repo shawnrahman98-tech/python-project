e3dcacc1ca396ac3c5ca6dc4518ce2df48844a9f
Shawn Rahman <shawn.rahman98@gmail.com>
2025-12-30 19:29:47 +0600

    Add files via upload

---
+++ Python word Guessing Game Project.py
@@ -0,0 +1,28 @@
+word  = "Shingaras"
+chances = 6
+GuessAdd = []
+done = False
+
+while not done:
+    for letter in word:
+        if letter.lower() in GuessAdd:
+            print(letter, end = " ")
+        else:
+            print("_", end = "")
+
+    MyGuess = input(f"Your Changes Is {chances}, Guess The Letter: ")
+    GuessAdd.append(MyGuess.lower())
+    if MyGuess.lower() not in word.lower():
+        chances -= 1
+        if chances == 0:
+            break
+
+    done = True
+    for letter in word:
+        if letter.lower() not in GuessAdd:
+            done = False
+
+if done:
+    print(f"yes, you have won the game, the word is {word} ")
+else:
+    print("you loss the game")
